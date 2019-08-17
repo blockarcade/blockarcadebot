1e6b2072cc38390ea85abd0cc27642b2bcd8b786
Jason Stallings <jason@stallin.gs>
2019-08-16 20:16:53 -0500

    feat: Add LOL to jackpot.

--- bot.js
+++ bot.js
@@ -148,12 +148,21 @@ const postJackpotToTelegram = () => {
             console.log(err);
             return;
           }
+          
+          const body = JSON.parse(response);
+          const tixBalance = body.balance;
+  
+          iostRequest('/getTokenBalance/ContractEnn4aBKJKwqQCsQiqFYovWWqm6vnA6xV1tT1YH5jKKpt/lol/true', (err, response) => {
+            if (err) {
+              console.log(err);
+              return;
+            }
 
-        const body = JSON.parse(response);
-        const iplayBalance = body.balance;
-        const cashGif = cashGifs[Math.floor(Math.random() * cashGifs.length)];
-        postGifToTelegram(cashGif, `*Major jackpot is up to ${numberWithCommas((iostBalance / 10).toFixed(2))} $IOST, ${numberWithCommas((itrxBalance/ 10).toFixed(2))} $ITRX, ${numberWithCommas((tixBalance / 10).toFixed(2))} $TIX and ${numberWithCommas((iplayBalance / 10).toFixed(2))} $IPLAY!*\n\nWho's going to win it?\n\nPlay now at: https://blockarca.de`);
-      
+          const body = JSON.parse(response);
+          const lolBalance = body.balance;
+          const cashGif = cashGifs[Math.floor(Math.random() * cashGifs.length)];
+          postGifToTelegram(cashGif, `*Major jackpot is up to ${numberWithCommas((iostBalance / 10).toFixed(2))} $IOST, ${numberWithCommas((itrxBalance/ 10).toFixed(2))} $ITRX, ${numberWithCommas((tixBalance / 10).toFixed(2))} $TIX, ${numberWithCommas((iplayBalance / 10).toFixed(2))} $IPLAY and ${numberWithCommas((lolBalance / 10).toFixed(2))} $IPLAY!*\n\nWho's going to win it?\n\nPlay now at: https://blockarca.de`);
+        });
       });
       });
     });
