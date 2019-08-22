56bb15fec243fdc754e6ca83a5cc3611b3c961b2
Jason Stallings <jason@stallin.gs>
2019-08-22 18:48:38 -0500

    fix: Make renderRanking async.

--- dappRanking.js
+++ dappRanking.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-const renderRanking = () => {
+const renderRanking = async () => {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.setViewport({ width: 375, height: 667 });
