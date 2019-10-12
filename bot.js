e0800b560d92fea70804ef2b4016d5d2f08179ac
Jason Stallings <jason@stallin.gs>
2019-10-12 11:00:10 -0500

    feat: Next airdrop!!

--- bot.js
+++ bot.js
@@ -190,7 +190,7 @@ const postRegisteredUsers = () => {
       const filteredKeys = newKeys.filter(el => el != null);
       
       postToTelegram(
-        `💰💰 There are *${filteredKeys.length}* $IOST accounts eligible for the next AIRDROP! 20,000 $TIX airdrop happening on October 12th 💰💰\n\nTo be eligible you need to be active in this room within a week before the airdrop.`,
+        `💰💰 There are *${filteredKeys.length}* $IOST accounts eligible for the next AIRDROP! 20,000 $TIX airdrop happening on October 29th 💰💰\n\nTo be eligible you need to be active in this room within a week before the airdrop.`,
         undefined,
         true
       );
